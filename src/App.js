fa09b2d13b9aeade41f372bd88129aae34a3a92d
LuisGzlz <sam.luisgonzalez@gmail.com>
2023-05-01 10:35:56 -0300

    ultimo commit del proyecto del curso basico de React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,158 @@
-
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import './App.css';
-import Header from './componentes/Header/Header.js';
-import Formulario from './componentes/Formulario/Formulario.js';
+import Header from "./componentes/Header/Header"
+import Formulario from './componentes/Formulario/Formulario';
+import MiOrg from './componentes/MiOrg';
+import Equipo from './componentes/Equipo';
+import Footer from './componentes/Footer';
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar] = useState(true)
+  const [colaboradores, actualizarColaboradores] = useState([{
+    id : uuid(),
+    equipo: "Front End",
+    foto: "https://github.com/harlandlohora.png",
+    nombre: "Harland Lohora",
+    puesto: "Instructor",
+    fav: true
+  },
+  {
+    id : uuid(),
+    equipo: "Front End",
+    foto: "https://github.com/LuisMGonz.png",
+    nombre: "Luis González",
+    puesto: "Instructor",
+    fav: true
+  }])
+
+  const [equipos, actualizarEquipos] = useState([
+    {
+      id : uuid(),
+      titulo: "Programación",
+      colorPrimario: "#57C278",
+      colorSecundario: "#D9F7E9"
+    },
+    {
+      id : uuid(),
+      titulo: "Front End",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF"
+    },
+    {
+      id : uuid(),
+      titulo: "Data Science",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2"
+    },
+    {
+      id : uuid(),
+      titulo: "Devops",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8"
+    },
+    {
+      id : uuid(),
+      titulo: "UX y Diseño",
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5"
+    },
+    {
+      id : uuid(),
+      titulo: "Móvil",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9"
+    },
+    {
+      id : uuid(),
+      titulo: "Innovación y Gestión",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF"
+    }
+  ])
+
+  //Ternario --> condicion ? seMuestra : noSeMuestra
+  // condicion && seMuestra
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario)
+  }
+
+  //Registrar colaborador
+
+  const registrarColaborador = (colaborador) => {
+    console.log("Nuevo colaborador", colaborador)
+    //Spread operator
+    actualizarColaboradores([...colaboradores, colaborador])
+  }
+
+//Eliminar Colaborador
+  const eliminarColaborador = (id) => {
+    console.log('Eliminar Colaborador:', id);
+    const nuevosColaboradores = colaboradores.filter((colaborador) => colaborador.id !== id)
+    actualizarColaboradores(nuevosColaboradores);
+    
+      }
+
+// Actualizar color de equipo
+  const actualizarColor = (color, id) =>{
+    console.log('actualizar color:', color, id);
+    const equiposActualizados = equipos.map((equipo) =>{
+      if(equipo.id === id){
+        equipo.colorPrimario = color
+      }
+      return equipo 
+    })
+    actualizarEquipos(equiposActualizados)
+  }
+
+  //Crear Equipo
+  const crearEquipo = (nuevoEquipo) => {
+    console.log(nuevoEquipo);
+    actualizarEquipos([...equipos, { ...nuevoEquipo, id: uuid()}])
+    
+  }
+
+  //Likes!
+  const like = (id) => {
+    console.log('like', id);
+    const colaboradoresActualizados = colaboradores.map((colaborador) => {
+      if (colaborador.id === id){
+        colaborador.fav = !colaborador.fav
+      }
+      return colaborador
+    })
+    actualizarColaboradores(colaboradoresActualizados)
+  }
+
+
   return (
-    <div className="App">
+    <div>
       <Header />
-      <Formulario />
+      {/* {mostrarFormulario ? <Formulario /> : <></>} */}
+      {
+        mostrarFormulario && <Formulario
+          equipos={equipos.map((equipo) => equipo.titulo)}
+          registrarColaborador={registrarColaborador}
+          crearEquipo={crearEquipo}
+        />
+      }
+
+      <MiOrg cambiarMostrar={cambiarMostrar} />
+
+      {
+        equipos.map((equipo) => <Equipo
+          datos={equipo}
+          key={equipo.titulo}
+          colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)}
+          eliminarColaborador={eliminarColaborador}
+          actualizarColor={actualizarColor}
+          like={like}
+        />
+        )
+      }
+
+      <Footer />
 
 
     </div>
@@ -15,4 +160,3 @@ function App() {
 }
 
 export default App;
-
--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -2,9 +2,8 @@ import "./Header.css"
 
 function Header() {
     return <header className="header">
-        <img src="/img/Header.png" alt="Encabezado Curso React"/>
+        <img src="/img/header.png" alt='Org' />
     </header>
 }
-
 
 export default Header
